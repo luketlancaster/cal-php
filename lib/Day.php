f1f36c2b31fa71ffb6869e2c1f8ce208f12d9ead
Luke Lancaster <luketlancaster@gmail.com>
2016-04-27 13:58:32 -0500

    zellers returning proper day number

---
+++ lib/Day.php
@@ -0,0 +1,45 @@
+<?php
+
+
+/* Zeller's Congruence Rules:
+ * h = day of week (0 = Sunday)
+ * q = day of month
+ * m = month (4 = April)
+ * Y = year
+*/
+
+
+class Day
+{
+
+    private $_year;
+    private $_day_of_month;
+    private $_month;
+
+    public function __construct($year, $month)
+    {
+        $this->_year = $year;
+        $this->_day_of_week = 1;
+        $this->_month = $month;
+    }
+
+    public function day_of_week()
+    {
+        return ($this->_zellers()) % 7;
+    }
+
+    private function _zellers()
+    {
+        $q = 1;
+        $m = $this->_month;
+        $Y = $this->_year;
+
+        if ($m < 3) {
+            $Y -= 1;
+            $m += 12;
+        }
+
+        $h = ($q + floor(((($m + 1)* 26) / 10)) + $Y + floor($Y/4) + (6 * floor($Y/100)) + floor($Y/400)  );
+        return $h;
+    }
+}
